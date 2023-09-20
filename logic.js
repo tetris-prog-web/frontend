ea9257ddc26b1f06d5e938a27ee602414bc60ffc
niiandreatti <nicolasandreatti5@gmail.com>
2023-09-20 10:09:06 -0300

    logica

---
+++ logic.js
@@ -0,0 +1,48 @@
+const iPosition = [
+    [0, grid, grid*2, grid*3],
+    [0, 1, 2, 3],
+    [0, grid, grid*2, grid*3],
+    [0, 1, 2, 3],
+]
+
+const oPosition = [
+    [0, 1, grid, grid + 1],
+    [0, 1, grid, grid + 1],
+    [0, 1, grid, grid + 1],
+    [0, 1, grid, grid + 1],
+]
+
+const tPosition = [
+    [1, 2, grid + 1, grid*2 + 1],
+    [grid, grid + 1, grid + 2, grid*2 + 2],
+    [1, grid + 1, grid*2, grid*2 + 1],
+    [grid, grid*2, grid*2 + 1, grid*2 + 2]
+]
+
+const lPosition = [
+    [0, grid, grid*2, grid*2 + 1],
+    [grid, grid + 1, grid + 2, 2],
+    [0, 1, grid +1, grid*2 + 1],
+    [0, 1, 2, grid],
+]
+
+const yPosition = [
+    [grid, grid + 1, grid + 2, 1],
+    [1,grid + 1, grid*2 + 1, grid],
+    [0, 1, 2, grid + 1],
+    [0, grid, grid*2, grid + 1],
+]
+
+const uPosition = [
+    [0, grid, grid + 1, grid + 2, 2],
+    [0, 1, grid + 1, grid*2 + 1, grid*2],
+    [grid, 0, 1, 2, grid + 2],
+    [1, 0, grid, grid*2, grid*2 + 1],
+]
+
+const positions = [uPosition,yPosition,lPosition,tPosition,oPosition, iPosition]
+
+let Rotation = 0
+let random = Math.floor(Math.random() * positions.length)
+let Shape = positions[random][Rotation]
+let $grid = Array.from(document.querySelectorAll(".next-piece"))
