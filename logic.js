a3f6bab4c9fbde944b3e63f922c3e21412066e79
niiandreatti <nicolasandreatti5@gmail.com>
2023-10-06 10:52:12 -0300

    nova-logica

--- logic.js
+++ logic.js
@@ -18,14 +18,29 @@ function mostrarModal() {
     fecharBotao.addEventListener('click', fecharModal);
   }
 
+
+
 // modal
 
-const gridWidth = 10;
-
-const colors = [ "yellow"]
 
 
-const iPosition = [
+document.addEventListener("DOMContentLoaded", function () {
+  
+
+  const gridContainer = document.getElementById('game-area-container');
+  const squares = Array.from(gridContainer.querySelectorAll(".game-area div"));
+
+  const grid = 10;
+
+  const color = ['yellow','orange',
+  'red'];
+
+  const transparency = 0.45;
+
+  const transparentColor = [
+    `rgba(255, 255, 0, ${transparency})`,
+  ]
+  const iPosition = [
     [0, grid, grid*2, grid*3],
     [0, 1, 2, 3],
     [0, grid, grid*2, grid*3],
@@ -67,22 +82,111 @@ const uPosition = [
     [1, 0, grid, grid*2, grid*2 + 1],
 ]
 
-const positions = [uPosition,yPosition,lPosition,tPosition,oPosition, iPosition]
-let start = 10
-let Rotation = 0
-let random = Math.floor(Math.random() * positions.length)
-let shape = positions[random][Rotation]
-let $grid = Array.from(document.querySelectorAll(".game-area div"))
 
-function draw() {
-  shape.forEach(index => {
-    $grid[index + start].classList.add("shapePainted", `${colors}`)
-  })
-}
-draw()
+  const positions = [uPosition, yPosition, lPosition, tPosition, oPosition, iPosition];
+  let random = randomShape(); 
+  let start = 10;
+  let Rotation = 0;
+  
 
-function undraw() {
-  shape.forEach(index => {
-    $grid[index + start].classList.remove("shapePainted", `${colors}`)
-  })
-}
+  let currentRot = 0;
+  let current = positions[random][currentRot];
+
+  
+  function draw() {
+    showPiece();
+    current.forEach(index => {
+      squares[start + index].classList.add("shapePainted");
+      squares[start + index].style.backgroundColor = color[random];
+    });
+  }
+
+  draw();
+
+  function undraw() {
+    hidePiece();
+    current.forEach(index => {
+      squares[start + index].classList.remove("shapePainted");
+      squares[start + index].style.backgroundColor = "";
+    });
+  }
+
+  function showPiece() {
+    let previewPos = start;
+    while (!current.some(index => squares[previewPos + index + grid].classList.contains("shapePainted"))) {
+      previewPos += grid;
+    }
+
+    current.forEach(index => {
+      if (!squares[previewPos + index + grid].classList.contains("shapePainted")) {
+        squares[previewPos + index].style.backgroundColor = transparentColor;
+      }
+    });
+  }
+
+  function hidePiece() {
+    current.forEach(index => {
+      squares[start + index].style.backgroundColor = "";
+    });
+  }
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(index => (start + index) % grid === 0);
+  
+    if (!isAtLeftEdge) {
+      start -= 1; 
+    }
+  
+    if (current.some(index => squares[start + index].classList.contains("shapePainted"))) {
+      start += 1; 
+    }
+  
+    draw();
+  }
+  
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(index => (start + index + 1) % grid === 0);
+  
+    if (!isAtRightEdge) {
+      start += 1; 
+    }
+  
+    if (current.some(index => squares[start + index].classList.contains("shapePainted"))) {
+      start -= 1; 
+    }
+  
+    draw();
+  }
+
+  function rotate() {
+    undraw();
+    let newRotation = (Rotation + 1) % positions[random].length;
+  
+    const newShape = positions[random][newRotation];
+  
+    const hasCollision = newShape.some(index => squares[start + index].classList.contains("shapePainted"));
+  
+    if (!hasCollision) {
+      Rotation = newRotation;
+      current = newShape;
+    }
+  }
+  function control(e){
+    if (e.key === 'ArrowLeft') {
+      moveLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveRight();
+    } else if (e.key === 'ArrowDown') {
+      // Implemente a lógica para mover a peça para baixo
+    } else if (e.key === 'ArrowUp') {
+      rotate();
+    }
+  }
+  
+  function randomShape(){
+      return Math.floor(Math.random() * positions.length);
+    }
+    
+    draw(); 
+});
