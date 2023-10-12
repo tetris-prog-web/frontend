f9d08b4aad3c7f73c4665ece7da4965ff0252382
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2023-10-12 13:50:31 -0300

    fix indenting

--- scripts/logic-extended.js
+++ scripts/logic-extended.js
@@ -3,50 +3,50 @@ document.addEventListener("DOMContentLoaded", function () {
     const colors = ["blue", "yellow", "red", "orange", "pink"]
     let currentColor = Math.floor(Math.random() * colors.length);
     let nextColor = Math.floor(Math.random() * colors.length)
-    const grid = 22;  
+    const grid = 22;
     let gameStarted = false;
 
-const iPosition = [
-    [0, grid, grid * 2, grid * 3],
-    [0, 1, 2, 3],
-    [0, grid, grid * 2, grid * 3],
-    [0, 1, 2, 3],
-];
+    const iPosition = [
+        [0, grid, grid * 2, grid * 3],
+        [0, 1, 2, 3],
+        [0, grid, grid * 2, grid * 3],
+        [0, 1, 2, 3],
+    ];
 
-const oPosition = [ 
-    [0, 1, grid, grid + 1],
-    [0, 1, grid, grid + 1],
-    [0, 1, grid, grid + 1],
-    [0, 1, grid, grid + 1],
-];
+    const oPosition = [
+        [0, 1, grid, grid + 1],
+        [0, 1, grid, grid + 1],
+        [0, 1, grid, grid + 1],
+        [0, 1, grid, grid + 1],
+    ];
 
-const tPosition = [
-    [0, 1, grid, grid * 2],
-    [0, grid, grid + 1, grid + 2],
-    [1, grid + 1, grid * 2 + 1, grid * 2],
-    [0, 1, 2, grid + 2]
-];
+    const tPosition = [
+        [0, 1, grid, grid * 2],
+        [0, grid, grid + 1, grid + 2],
+        [1, grid + 1, grid * 2 + 1, grid * 2],
+        [0, 1, 2, grid + 2]
+    ];
 
-const lPosition = [
-    [0, grid, grid * 2, grid * 2 + 1],
-    [grid, grid + 1, grid + 2, 2],
-    [0, 1, grid + 1, grid * 2 + 1],
-    [0, 1, 2, grid]
-];
+    const lPosition = [
+        [0, grid, grid * 2, grid * 2 + 1],
+        [grid, grid + 1, grid + 2, 2],
+        [0, 1, grid + 1, grid * 2 + 1],
+        [0, 1, 2, grid]
+    ];
 
-const yPosition = [
-    [1, grid, grid + 1, grid + 2],
-    [1, grid + 1, grid * 2 + 1, grid],
-    [0, 1, 2, grid + 1],
-    [0, grid, grid * 2, grid + 1]
-];
+    const yPosition = [
+        [1, grid, grid + 1, grid + 2],
+        [1, grid + 1, grid * 2 + 1, grid],
+        [0, 1, 2, grid + 1],
+        [0, grid, grid * 2, grid + 1]
+    ];
 
-const uPosition = [
-    [0, grid, grid + 1, grid + 2, 2],
-    [0, 1, grid + 1, grid * 2 + 1, grid * 2],
-    [grid, 0, 1, 2, grid + 2],
-    [1, 0, grid, grid * 2, grid * 2 + 1]
-];
+    const uPosition = [
+        [0, grid, grid + 1, grid + 2, 2],
+        [0, 1, grid + 1, grid * 2 + 1, grid * 2],
+        [grid, 0, 1, 2, grid + 2],
+        [1, 0, grid, grid * 2, grid * 2 + 1]
+    ];
 
 
     const positions = [uPosition, yPosition, lPosition, tPosition, oPosition, iPosition];
@@ -70,22 +70,23 @@ const uPosition = [
             squares[start + index].classList.remove("shapePainted", `${colors[currentColor]}`);
         });
     }
+
     const minisquares = document.querySelectorAll(".next-piece div");
     const miniWidth = 4;
     const nextPosition = 2;
     const nextShapeIndices = [
-    [1, 2, miniWidth + 1, miniWidth * 2 + 1], 
-    [miniWidth + 1, miniWidth + 2, miniWidth * 2, miniWidth * 2 + 1], 
-    [1, miniWidth, miniWidth + 1, miniWidth + 2], 
-    [0, 1, miniWidth, miniWidth + 1], 
-    [1, miniWidth + 1, miniWidth * 2 + 1, miniWidth * 3 + 1] 
-];
-    
+        [1, 2, miniWidth + 1, miniWidth * 2 + 1],
+        [miniWidth + 1, miniWidth + 2, miniWidth * 2, miniWidth * 2 + 1],
+        [1, miniWidth, miniWidth + 1, miniWidth + 2],
+        [0, 1, miniWidth, miniWidth + 1],
+        [1, miniWidth + 1, miniWidth * 2 + 1, miniWidth * 3 + 1]
+    ];
+
     function displayNextShape() {
         let nextRandomShape = Math.floor(Math.random() * nextShapeIndices.length)
         const nextShape = nextShapeIndices[nextRandomShape]
-        nextShape.forEach(squareIndex => 
-            minisquares[squareIndex + nextPosition + miniWidth].classList.add("shapePainted")  
+        nextShape.forEach(squareIndex =>
+            minisquares[squareIndex + nextPosition + miniWidth].classList.add("shapePainted")
         )
     }
 
@@ -94,7 +95,7 @@ const uPosition = [
             gameStarted = true;
         }
     });
-    
+
     document.addEventListener("keydown", (event) => {
         if (gameStarted) {
             if (event.key === "ArrowLeft") {
@@ -122,18 +123,18 @@ const uPosition = [
             timerId = null;
         } else {
             moveDown();
-            timerId = setInterval(moveDown, 1000); 
+            timerId = setInterval(moveDown, 1000);
         }
     });
 
     const $restartButton = document.getElementById("restart-button")
     $restartButton.addEventListener("click", () => {
-    window.location.reload()
-})
+        window.location.reload()
+    })
 
     function moveLeft() {
         undraw();
-        
+
         const isAtLeftEdge = current.some(index => (start + index) % grid === 0);
 
         if (!isAtLeftEdge) {
@@ -144,6 +145,7 @@ const uPosition = [
 
         draw();
     }
+
     function moveRight() {
         undraw();
         const isAtRightEdge = current.some(index => (start + index + 1) % grid === 0);
@@ -170,41 +172,41 @@ const uPosition = [
 
     function rotate() {
         undraw();
-        
+
         const newRotation = (Rotation + 1) % positions[random].length;
         const newShape = positions[random][newRotation];
-        
+
         const isLeftEdgeLimit = newShape.some(index => (start + index) % grid === -1);
         const isRightEdgeLimit = newShape.some(index => (start + index) % grid === 21);
         const isFilled = newShape.some(index => squares[start + index].classList.contains("busy"));
-    
+
         if (!isLeftEdgeLimit && !isRightEdgeLimit && !isFilled) {
             Rotation = newRotation;
             current = newShape;
         }
-        
+
         draw();
     }
 
     function checkIfRowIsFilled() {
         for (let row = 0; row < grid; row++) {
             const rowIndices = [];
-    
+
             for (let col = 0; col < grid; col++) {
                 rowIndices.push(row * grid + col);
             }
-    
+
             const isRowPainted = rowIndices.every(squareIndex =>
                 squares[squareIndex].classList.contains("shapePainted")
             );
-    
+
             if (isRowPainted) {
                 // Remove classes das células da linha
                 rowIndices.forEach(squareIndex => squares[squareIndex].classList.remove("shapePainted"));
-    
+
                 // Remove as células da linha da matriz
                 squares.splice(row * grid, grid);
-    
+
                 // Adicione as células de volta na parte superior
                 for (let i = 0; i < grid; i++) {
                     squares.unshift(document.createElement("div"));
@@ -213,7 +215,7 @@ const uPosition = [
             }
         }
     }
-    
+
     function stop() {
 
         if (current.some(index => squares[start + index + grid].classList.contains("busy"))) {
@@ -226,7 +228,7 @@ const uPosition = [
             random = randomShape();
             current = positions[random][Rotation];
             currentColor = nextColor;
-    
+
             draw();
             checkIfRowIsFilled();
             displayNextShape();
